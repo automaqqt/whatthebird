6b54334ef5b580e5a2a5558fc6da148b3afa5288
automaqqt <59531560+automaqqt@users.noreply.github.com>
2023-07-08 16:33:55 +0200

    dont crash processor

--- rpi/processor.py
+++ rpi/processor.py
@@ -1,12 +1,11 @@
 import cv2, os, time, torch, requests, pprint, base64
 import geocoder
-g = geocoder.ip('me')
 
 ### A class that acts as a processor
 class BirdCam():
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
-        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5l', pretrained=True)
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True)
         self.bird_api_url = "https://api.birds.einfachschach.de/v1/predict/birds"
         self.location = self.find_location()
 
@@ -15,19 +14,35 @@ class BirdCam():
 
     def process(self):
         while True:
-            result, image = self.cam.read()
-            if result:
-                results = self.model(image)
-                res = results.pandas()
-                if 'bird' in str(res.xyxy[0]):
-                    response = self.check_bird(image)
-                    pprint.pprint(response)
+            try:
+                result, image = self.cam.read()
+                if result:
+                    results = self.model(image)
+                    res = results.pandas()
+                    if 'bird' in str(res.xyxy[0]):
+                        self.wait_for__internet_connection()
+                        response = self.check_bird(image)
+                        pprint.pprint(response)
 
-            else:
-                print("No Cam available. Please! try again")
+                else:
+                    print("No Cam available. Please! try again")
+            except Exception as e:
+                print(f"Exception while processing: {e}")
+                time.sleep(2)
             time.sleep(1)
     
+    def wait_for__internet_connection(self):
+        while True:
+            try:
+                requests.get('https://google.com', timeout=5)
+                break
+            except requests.exceptions.ConnectionError:
+                print("No internet connection. Please connect!")
+                time.sleep(5)
+        
+    
     def find_location(self):
+        self.wait_for__internet_connection()
         g = geocoder.ip('me')
         print([g.city, g.latlng])
         return [str(ll) for ll in g.latlng]
